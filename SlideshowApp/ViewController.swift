a972fb1ac991254fa662ff406b579c277468e7dc
kaori <qbs0003@icloud.com>
2020-04-23 12:13:46 +0900

    初期表示画像追加

--- SlideshowApp/ViewController.swift
+++ SlideshowApp/ViewController.swift
@@ -9,10 +9,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var imageView1: UIImageView!
+    
+    var imageIndex = 0
+    
+    let images: [UIImage] = [UIImage(named:"sakura.jpg")!, UIImage(named:"sunset.jpg")!, UIImage(named:"hanabi.jpg")!]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        imageView1.image = images[imageIndex]
     }
     
     @IBAction func moveButton(_ sender: Any) {
